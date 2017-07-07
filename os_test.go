bb7c00901baaab51fff46adaf5ecb6ee5b2f3baf
aQua <aQua@aQuaYi.com>
2017-07-07 22:56:16 +0800

    修改了细节。

--- os_test.go
+++ os_test.go
@@ -5,7 +5,7 @@ import "os"
 import "github.com/stretchr/testify/assert"
 
 func Test_PWD(t *testing.T) {
-	os.Chdir("/")
+	os.Chdir("/") // 改变目录为了适应不同的测试环境，比如，TravisCI
 	expected := "/"
 	assert.Equal(t, expected, PWD(), "获取了错误的PWD")
 }
